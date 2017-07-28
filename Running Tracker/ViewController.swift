ea8a2893d775475aac3c566807356d3600a856d2
KappaJohns <johnnychiu123@gmail.com>
2017-07-28 15:44:37 -0400

    first
    first commit

--- Running Tracker/ViewController.swift
+++ Running Tracker/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startTimer: UIButton!
+
+    @IBOutlet weak var stopTimer: UIButton!
+
+    @IBOutlet weak var distanceField: UITextField!
+    
+    @IBOutlet weak var timeField: UITextField!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "running_people_02.png")!)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
     }
 
 
+    @IBAction func startTimerButton(_ sender: Any){
+        
+        
+        
+    }
+    
+    
+    @IBAction func stopTimer(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
